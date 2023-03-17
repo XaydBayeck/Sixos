99f64a652e3328afd5c400e1d8087a28c02e9a96
Sid <SidBayeck@outlook.com>
2023-03-17 16:56:38 +0800

    add some packages

--- common/basic/tools.mod.nix
+++ common/basic/tools.mod.nix
@@ -6,5 +6,7 @@
     zathura # doc browzer
     crow-translate # dict, ocr, translater
     flameshot # screen cut
+    screenkey # display the key you press
+    xmodmap   # setting and query your modifier keys
   ];
 }

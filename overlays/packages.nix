227e9b824a446b508bca4b99acf6d78352848f67
Sid <SidBayeck@outlook.com>
2023-03-17 11:34:32 +0800

    add overlays section in flake outputs

---
+++ overlays/packages.nix
@@ -0,0 +1,15 @@
+self: super:
+
+with builtins;
+
+let
+  dirContents = readDir ./packages;
+
+  genPackage = name: {
+    inherit name;
+    value = self.callPackage (./packages + "/${name}") {};
+  };
+
+  names = attrNames dirContents;
+in
+  listToAttrs (map genPackage names)

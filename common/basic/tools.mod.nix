7249bff53fff93c8fd384905e825fe05c5122554
Sid <SidBayeck@outlook.com>
2023-03-17 16:57:55 +0800

    fix: wrong package name

--- common/basic/tools.mod.nix
+++ common/basic/tools.mod.nix
@@ -7,6 +7,6 @@
     crow-translate # dict, ocr, translater
     flameshot # screen cut
     screenkey # display the key you press
-    xmodmap   # setting and query your modifier keys
+    xorg.xmodmap   # setting and query your modifier keys
   ];
 }

c907676c23f40af4e16b9e3938b50e1c9dc11f6a
Sid <SidBayeck@outlook.com>
2023-03-18 23:54:12 +0800

    control grammars by hand

--- common/develop/nixvim/plugins/treesitter.nix
+++ common/develop/nixvim/plugins/treesitter.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   programs.nixvim.plugins = {
@@ -7,7 +7,80 @@
       ensureInstalled = [ "c" "rust" "lua" "haskell" "cpp" "html" "javascript" "typescript" "nix" ];
       folding = true;
       indent = true;
-      nixGrammars = true;
+
+      grammarPackages = with config.programs.nixvim.plugins.treesitter.package.builtGrammars; [
+        agda
+        arduino
+        awk
+        bash
+        bibtex
+        c
+        c_sharp
+        clojure
+        cmake
+        comment
+        commonlisp
+        cpp
+        css
+        cuda
+        diff
+        dockerfile
+        dot
+        elixir
+        elmerlang
+        fennel
+        fish
+        gdscript
+        git_rebase
+        gitcommit
+        gitattributes
+        gitignore
+        glsl
+        godot_resource
+        gomod
+        graphql
+        haskell
+        hlsl
+        http
+        html
+        help # neovim help
+        java
+        javascript
+        json
+        json5
+        julia
+        kotlin
+        latex
+        llvm
+        lua
+        make
+        markdown
+        markdown_inline
+        mermaid
+        nix
+        ocaml
+        ocaml_interface
+        ocamllex
+        regex
+        python
+        rust
+        ron
+        ruby
+        scala
+        scheme
+        solidity
+        typescript
+        tsx
+        toml
+        verilog
+        vim
+        vue
+        wgsl
+        wgsl_bevy
+        yaml
+        zig
+      ];
+
       incrementalSelection = {
         enable = true;
         keymaps = {

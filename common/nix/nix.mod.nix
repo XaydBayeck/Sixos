5d9b11502c39ff08fdfec47d003da67413eba5a6
Sid <SidBayeck@outlook.com>
2023-03-21 08:46:08 +0800

    Add `sid` to trusted users

--- common/nix/nix.mod.nix
+++ common/nix/nix.mod.nix
@@ -4,9 +4,12 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  nix.settings.substituters = [ 
-    "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" 
-  ];
+  nix.settings = {
+    substituters = [
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+    ];
+    experimental-features = [ "nix-command" "flakes" ];
+    trusted-users = [ "root" "sid" ];
+  };
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }

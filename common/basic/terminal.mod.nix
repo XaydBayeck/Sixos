ebd7fcf94bd3bca7779ee4f1c9a96268684d3821
Sid <SidBayeck@outlook.com>
2023-03-21 17:35:14 +0800

    add some pkgs

--- common/basic/terminal.mod.nix
+++ common/basic/terminal.mod.nix
@@ -5,7 +5,9 @@
     alacritty # fast GPU accelerate terminal
     tldr   # better manual Reader
     httpie # simple http client
+    rar    # archive rar file
     unar   # extract from rar file, free software replacement of unrar
+    zip    # archive zip file
     unzip  # extract from zip file
     p7zip  # support of 7z format, see 7-zip.org
     ripgrep# more faster

d35752d3d096278898df998695ea1bd5753a9d44
Sid <SidBayeck@outlook.com>
2023-03-21 18:58:03 +0800

    move fcitx5-nord in fcitx5-addons

--- common/locales/input.mod.nix
+++ common/locales/input.mod.nix
@@ -4,6 +4,10 @@
   # Select input method
   i18n.inputMethod = {
     enabled = "fcitx5";
-    fcitx5.addons = with pkgs; [ fcitx5-rime fcitx5-chinese-addons sinur.fcitx5-nord ];
+    fcitx5.addons = with pkgs; [
+      fcitx5-rime
+      fcitx5-chinese-addons
+      sinur.fcitx5-nord
+    ];
   };
 }

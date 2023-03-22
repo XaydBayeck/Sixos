857429517f1c834452f45e82e84d36f0915b8cd1
Sid <SidBayeck@outlook.com>
2023-03-22 12:12:08 +0800

    disable null-ls

--- fact/user/home/sid/nixvim/plugins/langs/nushell.nix
+++ fact/user/home/sid/nixvim/plugins/langs/nushell.nix
@@ -3,17 +3,17 @@
 {
   programs.nixvim = {
     extraPlugins = with pkgs.vimPlugins; [ nvim-nu ];
-    plugins.null-ls = {
-      enable = true;
-      extraOptions = {
-        sources = { __raw = ''
-            {
-              require("null-ls").builtins.completion.luasnip,
-              require("null-ls").builtins.completion.spell,
-              require("null-ls").builtins.diagnostics.fish,
-            }
-          ''; };
-      };
-    };
+    # plugins.null-ls = {
+    #   enable = true;
+    #   extraOptions = {
+    #     sources = { __raw = ''
+    #         {
+    #           require("null-ls").builtins.completion.luasnip,
+    #           require("null-ls").builtins.completion.spell,
+    #           require("null-ls").builtins.diagnostics.fish,
+    #         }
+    #       ''; };
+    #   };
+    # };
   };
 }

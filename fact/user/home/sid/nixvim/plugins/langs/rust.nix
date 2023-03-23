a06af0edf447e16e97743ace7f7b7a850a342b8c
Sid <SidBayeck@outlook.com>
2023-03-23 13:33:29 +0800

    add illuminate and set rainbow

--- fact/user/home/sid/nixvim/plugins/langs/rust.nix
+++ fact/user/home/sid/nixvim/plugins/langs/rust.nix
@@ -5,6 +5,7 @@
     rust-tools = {
       enable = true;
       inlayHints.parameterHintsPrefix = "::";
+      server.check.allTargets = false;
     };
     lsp.servers.rust-analyzer.enable = true;
   };
